3a170f1d4580004e732b77f910618de1168c6a2e
npoptomov <npoptomov@gmail.com>
2023-03-01 21:11:10 +0100

    Initial commit

---
+++ precache-manifest.8e7a1a8c36bacb4a88d3ee29998dbaca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ca26c892961314d636a3dd71df6569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2e9765a4b625c2c05a5",
+    "url": "/static/css/2.f0e33c08.chunk.css"
+  },
+  {
+    "revision": "de2f33bdcb01f168c225",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2e9765a4b625c2c05a5",
+    "url": "/static/js/2.d2cd98dd.chunk.js"
+  },
+  {
+    "revision": "70b7a62c5829b493bcadb4dc2a44d54a",
+    "url": "/static/js/2.d2cd98dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2f33bdcb01f168c225",
+    "url": "/static/js/main.c9cddd2e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.c9cddd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4a854c06bb36be3a7d",
+    "url": "/static/js/runtime-main.1530841d.js"
+  },
+  {
+    "revision": "3823b0bef3063438c356f06e527faa12",
+    "url": "/static/media/human.3823b0be.jpg"
+  }
+]);
